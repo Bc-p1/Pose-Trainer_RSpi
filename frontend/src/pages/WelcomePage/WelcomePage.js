ae7c7973ff982d4399d3507ee507a91d01c48d08
Bc-p1 <bibincp46@gmail.com>
2023-05-13 08:15:34 +0530

    pages added and designed

--- frontend/src/pages/WelcomePage/WelcomePage.js
+++ frontend/src/pages/WelcomePage/WelcomePage.js
@@ -3,21 +3,24 @@ import { Link } from 'react-router-dom'
 import './WelcomePage.css'
 export default function WelcomePage() {
     return (
-        <div className="WelcomePage-header">
-            <div className="text-center">
+        <div className="welcome-container">
+        <div className="welcome-box">
             <h1 className="main-title home-page-title">welcome to our app</h1>
-            </div>
+            //<img src="/utils/images/icon.svg" alt="App Icon" className="app-icon" />
+         </div>
             <p>
+            <div className="welcome-card">
             <div className="btn-section ">
             <Link to="/Register">
-                <button className="primary-button">Create Account</button>
+                <button className="welcome-button">Create Account</button>
             </Link>
             <Link to="/Signin">
-                <button className="primary-button">Login</button>
+                <button className="welcome-button">Login</button>
             </Link>
             <Link to="/">
-                <button className="primary-button">Log out</button>
+                <button className="welcome-button">Log out</button>
             </Link>
+            </div>
             </div>
             </p>
             
